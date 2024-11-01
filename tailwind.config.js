5ab8062b0110c9e64a5d880ebc6fe858febdf653
Alireza Joz <alireza00joz@gmail.com>
2024-11-01 16:45:39 +0330

    first commit and install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+        bg_log: "#242A38",
+      },
+      boxShadow: {
+        norml: "0px 1px 10px 0px rgba(0, 0, 0, 0.05)",
+      },
+      spacing: {
+        25: "6.25rem",
+        30: "7.5rem",
+        50: "12.5rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        "iran-B": "Iran Bold",
+        "iran-Med": "Iran medum",
+        "iran-R": "Iran  regular",
+        "iran-th": "Iran thin",
+        "iran-num": "IranFaNum",
+        "iran-black": "Iran Block",
+        "iran-light": "Iran Light",
+      },
+      letterSpacing: {
+        tightest: "-0.065em",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          lg: "0.625 rem",
+        },
+      },
+      backgroundImage: {},
+    },
+    screens: {
+      xs: "375px",
+      sm: "500px",
+      md: "760px",
+      lg: "970px",
+      xl: "1280px",
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "&>*");
+      addVariant("child-hover", "&>*:hover");
+    },
+    // require("tailwind-scrollbar"),
+  ],
+};
