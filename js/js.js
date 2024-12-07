d4eabd37a8a2fd7b1bb18c5a75420d115b7f01e1
Alireza Joz <alireza00joz@gmail.com>
2024-12-08 01:34:02 +0330

    ✅ add them dark and lghit

--- js/js.js
+++ js/js.js
@@ -19,3 +19,27 @@ let currentIndex = 0;
     }
 
     window.addEventListener('resize', updateSlider);
+
+
+    const themeToggle = document.getElementById('theme-toggle');
+
+  // تغییر تم با کلیک روی آیکون
+
+  if (
+    localStorage.getItem('theme') === 'dark' || 
+    (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)
+  ) {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
+  }
+  
+  themeToggle.addEventListener('click', () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light'); // ذخیره تم روشن
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark'); // ذخیره تم تاریک
+    }
+  });
