7f6a37af8f8b35c09349e3102449622046537c2d
Alireza Joz <alireza00joz@gmail.com>
2024-12-08 17:47:14 +0330

    🐛 set fixed them

--- js/js.js
+++ js/js.js
@@ -25,31 +25,45 @@ let togglebtn = document.querySelectorAll(".btnThem");
 
 togglebtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    let icons = btn.querySelector(".icon-light");
-    let changeIcon = document.querySelectorAll(".icon-light");
-    let c = document.querySelector(".changeName");
+    let changeIcons = document.querySelectorAll(".icon-light");
+    let c = document.querySelector(".changeName"); 
 
+    
     if (localStorage.theme === "dark") {
       document.documentElement.classList.remove("dark");
-      changeIcon.forEach(function (item) {
-        item.setAttribute("href", "#moon");
+      localStorage.setItem("theme", "light"); 
+      changeIcons.forEach(function (icon) {
+        icon.setAttribute("href", "#sun"); 
       });
-
-      c.innerHTML = "تم تیره";
-      localStorage.theme = "light";
+      c.innerHTML = "تم روشن"; 
     } else {
+      
       document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-      changeIcon.forEach(function (item) {
-        item.setAttribute("href", "#sun");
+      localStorage.setItem("theme", "dark"); 
+      changeIcons.forEach(function (icon) {
+        icon.setAttribute("href", "#moon"); 
       });
-
-      c.innerHTML = "تم روشن";
-      localStorage.theme = "dark";
+      c.innerHTML = "تم تیره"; 
     }
   });
-}); 
-// 
+});
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.add("dark");
+    document.querySelectorAll(".icon-light").forEach((icon) => {
+      icon.setAttribute("href", "#moon");
+    });
+    document.querySelector(".changeName").innerHTML = "تم تیره";
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.querySelectorAll(".icon-light").forEach((icon) => {
+      icon.setAttribute("href", "#sun");
+    });
+    document.querySelector(".changeName").innerHTML = "تم روشن";
+  }
+});
 
 // meno
 window.addEventListener('scroll', function() {
