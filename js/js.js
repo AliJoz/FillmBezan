eff6912e6f7cc4a2f5115933b294efb8742952f3
Alireza Joz <alireza00joz@gmail.com>
2024-12-14 23:57:31 +0330

    ✅ set timer and slider

--- js/js.js
+++ js/js.js
@@ -1,27 +1,89 @@
-let currentIndex = 0;
+let currentIndex = 0; 
+const itemsPerSlide = 4; 
+let autoSlideInterval;
+function updateSlider() {
+    const slider = document.getElementById('slider');
+    const slideWidth = slider.querySelector('.slide').clientWidth;
+    slider.style.transform = `translateX(${currentIndex * slideWidth}px)`;
+}
 
-    function updateSlider() {
-        const slider = document.getElementById('slider');
-        const slideWidth = slider.querySelector('.slide').clientWidth;
-        slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+function nextSlide() {
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+   
+    if (currentIndex + itemsPerSlide < totalSlides) {
+        currentIndex += 1;
+    } else {
+        currentIndex = 0; 
+    updateSlider();
+}
+function prevSlide() {
+    const totalSlides = document.querySelectorAll('.slide').length;
+    if (currentIndex > 0) {
+        currentIndex -= 1; 
+    } else {
+        currentIndex = totalSlides - itemsPerSlide;
     }
+    updateSlider();
+}
 
-    function prevSlide() {
-        const totalSlides = document.querySelectorAll('.slide').length;
-        currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalSlides - 1;
-        updateSlider();
+function resetAutoSlide() {
+    clearInterval(autoSlideInterval); 
+    startAutoSlide(); 
+}
+function startAutoSlide() {
+    autoSlideInterval = setInterval(() => {
+        nextSlide();
+    }, 5000); 
+}
+window.addEventListener('resize', updateSlider);
+startAutoSlide();
+
+
+    // btn them 
+let togglebtn = document.querySelectorAll(".btnThem");
+
+togglebtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let changeIcons = document.querySelectorAll(".icon-light");
+    let c = document.querySelector(".changeName"); 
+
+    
+    if (localStorage.theme === "dark") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light"); 
+      changeIcons.forEach(function (icon) {
+        icon.setAttribute("href", "#sun"); 
+      });
+      c.innerHTML = "تم روشن"; 
+    } else {
+      
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark"); 
+      changeIcons.forEach(function (icon) {
+        icon.setAttribute("href", "#moon"); 
+      });
+      c.innerHTML = "تم تیره"; 
     }
-
-    function nextSlide() {
-        const totalSlides = document.querySelectorAll('.slide').length;
-        currentIndex = (currentIndex < totalSlides - 1) ? currentIndex + 1 : 0;
-        updateSlider();
-    }
-
-    window.addEventListener('resize', updateSlider);
+  });
+});
 
 
-
+window.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.theme === "dark") {
+    document.documentElement.classList.add("dark");
+    document.querySelectorAll(".icon-light").forEach((icon) => {
+      icon.setAttribute("href", "#moon");
+    });
+    document.querySelector(".changeName").innerHTML = "تم تیره";
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.querySelectorAll(".icon-light").forEach((icon) => {
+      icon.setAttribute("href", "#sun");
+    });
+    document.querySelector(".changeName").innerHTML = "تم روشن";
+  }
+});
 // meno
 window.addEventListener('scroll', function() {
     var menu = document.getElementById('menu');
@@ -31,3 +93,5 @@ window.addEventListener('scroll', function() {
       menu.classList.remove('hiddenMeno');  
     }
   });
+  
+  }
